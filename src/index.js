a308eca180f0b2c42aeb0c88e129d7188ba34195
Nguyen Van Nhat Huy <jacksonnguyendev@gmail.com>
2018-05-31 13:23:01 +0700

    áád

--- src/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-
-import App from './App';
-import './vendor/bootstrap/css/bootstrap.min.css';
-import './index.css';
-
-ReactDOM.render(<App />, document.getElementById('root'));
